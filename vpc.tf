ac4a6c661fd2aead1681eb497b8473cfd999fc87
root <mehta.rajan88@gmail.com>
2023-03-30 16:12:10 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -2,6 +2,8 @@ module "network" {
   source  = "app.terraform.io/alphatf1604/network/google"
   version = "3.4.0"
   # insert required variables here
+  project_id   = var.project
+  network_name = "rajan-network"
   subnets = [
   {
     subnet_name   = "gaurav-subnet"
